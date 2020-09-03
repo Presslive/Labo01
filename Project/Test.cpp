90fc7b647128b7ac3d6437a7b2a48a6c00457f1d
Diallo Abdoulaye <70491998+Presslive@users.noreply.github.com>
2020-09-03 16:44:39 -0400

    Ajout de la conversion des caractères accentuées à partir de GitHub

--- Project/Test.cpp
+++ Project/Test.cpp
@@ -1,17 +1,19 @@
-// But: …crire un pprogramme qui affiche un message ‡ l'Ècran et apprendre ‡ gÈrer les versions du code
+// But: √âcrire un pprogramme qui affiche un message √† l'√©cran et apprendre √† g√©rer les versions du code
 // avec l'application git et de le publier dans le nuage avec GitHub
 // L'auteur:  Diallo Abdoulaye
 // Date: 2020-09-03
 
 
-#include<iostream>                       //IntËgre un coffre ‡ outils(coutcin,...0) pour communiquer avec l'utilisateur
+#include<iostream>                       //Int√®gre un coffre √† outils(coutcin,...0) pour communiquer avec l'utilisateur
 
 
-int main(/*liste des param;etes Èventuels */)
+int main(/*liste des param;etes √©ventuels */)
 {  //un bloc d'instruction
+	//pour g√©rer efficacement la convertion des caract√†res accenti√©es
+	setlocale(LC_ALL,"");
 
-     //afficher le message ‡ l'Ècran
-	std::cout << "Bienvenue ‡ tous ! ";
+     //afficher le message √† l'√©cran
+	std::cout << "Bienvenue √† tous ! ";
 
 
 
