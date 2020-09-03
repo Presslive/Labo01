658fa64883384d5d26ca1b916cf6a5fc3c0c65a6
Abdoulaye Diallo <2043337@cstj.qc.ca>
2020-09-03 15:56:28 -0400

    C'est le premier commit/ou validation du projet Lab01

---
+++ Project/Test.cpp
@@ -0,0 +1,5 @@
+// But: Écrire un pprogramme qui affiche un message à l'écran et apprendre à gérer les versions du code
+// avec l'application git et de le publier dans le nuage avec GitHub
+// L'auteur:  Diallo Abdoulaye
+// Date: 2020-09-03
+
